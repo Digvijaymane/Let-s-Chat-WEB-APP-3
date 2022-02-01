86c514c0c3ebfdb73048f9f50a92388c6fa519e4
Digvijaymane <87968107+Digvijaymane@users.noreply.github.com>
2022-02-01 17:26:16 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDcV0uBJELKCUry2iIinD-6k3r6R0IuqKs",
+    authDomain: "project-let-s-chat-web-app.firebaseapp.com",
+    databaseURL: "https://project-let-s-chat-web-app-default-rtdb.firebaseio.com",
+    projectId: "project-let-s-chat-web-app",
+    storageBucket: "project-let-s-chat-web-app.appspot.com",
+    messagingSenderId: "1002077145258",
+    appId: "1:1002077145258:web:05d9498a0b19481ad868d3"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
